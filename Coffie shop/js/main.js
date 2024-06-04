8b94e4bd7b4b230e10d28053fef675a7fa70363a
Bulat <bluat.vafin@mai.ru>
2024-06-04 23:36:51 +0300

    commit

---
+++ Coffie shop/js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".slider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    initialSlide: 1,
+});
